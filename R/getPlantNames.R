2c2cbbff4b816da1e129ae395d1cbb9fc3e2da54
johnpauls <johnpauls@operamail.com>
2018-03-19 11:45:15 -0400

    v. PlantNames_Fix 1
    - getPlantNames() no longer requires matching by TSN
    
    - created data.frame method
    
    - NPSForVeg method now just calls getCommons and then the data.frame method and no longer accessses the Commons slot directly
    
    - data.tables no longer used, but dplyr and purrr are
    
    -some updates to documentation
    
    - Closes #23

--- R/getPlantNames.R
+++ R/getPlantNames.R
@@ -1,35 +1,54 @@
+#' @include NPSForVeg_Class_def.R
+#' 
+#' @importFrom dplyr recode
+#' @importFrom purrr map_chr
+#' 
 #' @title getPlantNames
 #' 
-#' @import data.table
-#' 
-#' @description Translates Latin names to common names using TSN numbers
+#' @description Translates between Latin names, common names and TSN numbers
 #' 
 #' @param object An \code{NPSForVeg} object 
 #' @param names a character vector of Latin names, common names or TSN numbers (as a character vector) to be translated
 #' @param out.style Either "common", "Latin", or "TSN". Indicates the type of name that should be returned. Defaults to "common"
 #' @param in.style Either "common", "Latin", or "TSN". Indicates the type of name found in the "names" argument. 
-#' @param output  Currently only accpets "dataframe". Determines the output type When \code{object} is a list. "dataframe", the default, indicates the output from all of \code{NSPForVeg} objects should be combined into a single \code{data.frame}. When implemented "list" will return a \code{list} where each element of the list is a \code{data.frame} from a single \code{NPSForVeg} object, and each element is named based on that object's \code{ParkCode} slot. 
-
+#' @param output  Currently does nothtin Will accepts "vector" or "list". Determines the output type When \code{object} is a list 
+#'  When implemented "list" will return a \code{list} where each element of the list is a \code{data.frame} from a
+#'   single \code{NPSForVeg} object, and each element is named based on that object's \code{ParkCode} slot. 
 #' 
 #' @return A character vector of names in the format specified by \code{out.style}.
 #' 
-#' @details This function will convert the names in \code{names} to the style specified in \code{out.style}, using the data in the \code{Commons} slot of an object of class \code{NPSForVeg}. Note that TSN numbers are used to covert from one style of name to the other.  
+#' @details This function will convert the names in \code{names} to the style specified in \code{out.style},
+#'  using the data in the \code{Commons} slot of an object of class \code{NPSForVeg}. Note that if the data in the \code{Commons} slot
+#'  has more than one entry for one or more of the input names, the fucniton will fail with an error.
+#'   
 #' 
 #' @export
 
 
-setGeneric(name="getPlantNames",function(object,names, out.style="common", in.style="Latin",output="dataframe"){standardGeneric("getPlantNames")},signature="object" )
+setGeneric(name="getPlantNames",function(object,names, out.style="common", in.style="Latin",output="vector"){standardGeneric("getPlantNames")},signature="object" )
 
 setMethod(f="getPlantNames", signature="NPSForVeg",
     function(object,names,out.style, in.style) {
-      in.TSN<-switch(in.style,
-        TSN= names,
-        Latin= data.table(object@Commons,key="Latin_Name") [i=as.character(names),j=TSN],
-        common= data.table(object@Commons,key="NCRN_Common") [i=as.character(names),j=TSN]
-      )
-      switch(out.style,
-        TSN= return(in.TSN),
-        common= return(data.table(object@Commons,key="TSN")[i=in.TSN,j=Common]),
-        Latin= return(data.table(object@Commons,key="TSN")[i=in.TSN,j=Latin_Name])
-      )
+      
+      return(getPlantNames(object=getCommons(object), names=names, out.style = out.style, in.style = in.style))
+      
+})
+
+setMethod(f="getPlantNames", signature="data.frame",
+          function(object,names,out.style="common", in.style="Latin") {
+
+  # recode the text input to the correct column names
+            
+  in.col<-recode(in.style, Latin="Latin_Name", common="Common")     
+  out.col<-recode(out.style, Latin="Latin_Name", common="Common")  
+
+  # Prefitler the data so only the part that contains the correct names is examined 
+  
+  Prefilter<-object[object[[in.col]] %in% names,] 
+  
+  # Got through each name in input and get the correct ouptut
+  Output<-map_chr(names, ~Prefilter[Prefilter[[in.col]] %in% .x, out.col] )
+        
+  return(Output) 
+            
 })

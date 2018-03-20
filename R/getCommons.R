eba1844d61a6a1a1e8a9f18e9fc630af233e6e2a
johnpauls <johnpauls@operamail.com>
2018-03-20 15:53:20 -0400

    v PlantNames_fix v 4.
    - list method with data.frame output now returns a much smaller data.frame
    
    - added nativity filter
    
    - list method is much simpler
    
    - updated documentaiton
    
    - fixes #19

--- R/getCommons.R
+++ R/getCommons.R
@@ -1,47 +1,73 @@
+#' @include  NPSForVeg_Class_def.R
 #' @title getCommons
 #' 
-#' @importFrom dplyr filter
+#' @importFrom data.table rbindlist
+#' @importFrom dplyr distinct filter
 #' @importFrom rlang quo
 #' 
 #' @description gets the common names \code{data.frame} from one or more \code{NPSForVeg} objects. 
 #' 
-#' @param object Either a \code{NPSForVeg} object or a list of such objects.
+#' @param object Either a data.frame like that stored in the \code{Commons} slot, an \code{NPSForVeg} object or a list of such objects.
+#' @param species A character vector of species Latin names. Will only return data from those species.
+#' @param nativity Indicates the nativity of the desired species. Three options are available:
+#' \describe{
+#' \item{"native"}{Only native species are returned.}
+#' \item{"exotic"}{Only exotic species are returned.}
+#' \item{"all"}{The default. Species are returned regardless of nativity.}
+#' }
+#' 
 #' @param output Used when "object" is a list, to determine if the output is a \code{data.frame} or a \code{list}. Can be either "dataframe" (the default), or "list". Needs to be in quotes.
 #' 
 #' @return Either a \code{data.frame} or a \code{list} containing the contents of the "Commons" slot for one or more \code{NPSForVeg} objects.
 #'  
-#' @details  When the "object" is a single \code{NPSForVeg} object this function will return the \code{data.frame} in the "Commons" slot  which should have the Latin and common names of the plants as well as the TSN Numbers. When "object" is a \code{list} and "output" is "dataframe" a combied \code{data.frame} with the "Commons" slot from all NPSForVeg elements is returned. If the "object" is a \code{list} and "output" is "list" then a \code{list} is returned. Each element of the \code{list} will be the "Commons" slot from one the input \code{NPForVeg} objects, and the name of each element corresponds to the name of the \code{NPSForVeg} object it comes from.  
+#' @details  When the "object" is a single \code{NPSForVeg} object this function will return the \code{data.frame} in the "Commons" slot.  
+#' When "object" is a \code{list} and "output" is "dataframe" a combined \code{data.frame} with the "Commons" slot from all NPSForVeg 
+#' elements is returned. This \code{data.frame} is filtered so that identical rows are not returned. If a species is present in more than
+#' one of the \code{data.frame}s, and the metadata for the speices (e.g. native vs exotic) differs between them, then the species will 
+#' be returned twice, once with each set of metadata.  If the "object" is a \code{list} and "output" is "list" then a \code{list} is
+#' returned. Each element of the  \code{list} will be the "Commons" slot from one the input \code{NPForVeg} objects.  
 #' 
 #' @export
 
 ############### getCommons command for NPSForVeg class ###############
-setGeneric(name="getCommons",function(object,species=NA,output="dataframe"){standardGeneric("getCommons")},signature=c("object") )
+setGeneric(name="getCommons",function(object,species=NA,nativity="all",
+                                      output="dataframe"){standardGeneric("getCommons")},signature=c("object") )
 
 
 setMethod(f="getCommons", signature=c(object="list"),
-          function(object,output){
-            OutCommons<-vector(mode="list",length=length(object))
-            for (i in seq_along(object)) {
-              OutCommons[[i]]<-getCommons(object[[i]])
-            } 
-            switch(output,
-                   list={names(OutCommons)<-getNames(object,name.class="code")
-                         return(OutPCommons)},
-                   dataframe=return( do.call("rbind",OutCommons) )
-            )
+          function(object,species,nativity, output){
             
-            } )  
+            OutCommons<-lapply(object, getCommons, species, nativity)
+            
+            switch(output,
+                   list=return(OutCommons),
+                   dataframe=return( distinct(rbindlist(OutCommons) ))
+            )
+})  
 
 
 setMethod(f="getCommons", signature=c(object="NPSForVeg"),
-          function(object,species){
-                  return( getCommons(object@Commons, species) )
-          })
+          function(object,species, nativity){
+                  return( getCommons(object@Commons, species, nativity) )
+ })
 
 setMethod(f="getCommons", signature=c(object="data.frame"),
-          function(object, species=NA){
-            x<-list(quo(Latin_Name %in% species))
-            x<-x[all(!is.na(species))]
-            y<-if(length(x)>0) filter(object,!!!x) else object
-            return(y)
-          })
+          function(object, species, nativity){
+            
+            ## create "filt" which will hold all of the filters to be applied
+            filt<-list(quo(Latin_Name %in% species),  # species names
+                       switch(nativity,                 # exotic or not
+                              "all"=NA,
+                              "native"= quo(!Exotic),
+                              "exotic"=quo(Exotic)
+                          )
+                       
+                       
+            )
+     
+            ## get rid of filters that the user does not want
+            filt<-filt[c(all(!is.na(species)),!nativity=="all") ]
+     
+            # filter data or just return the data.frame if there are no filters
+            return( if(length(x)>0) filter(object, !!!filt) else object)
+  })

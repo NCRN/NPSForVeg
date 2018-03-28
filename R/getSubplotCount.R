270bc2656391c47af62aa7560c48dde900f8d45d
KateMMiller <kathryn_miller@nps.gov>
2018-03-28 09:30:59 -0400

    Improved filtering of getter file and made it more flexible. If a network doesn't have subplot sampling information in the Events.csv, the getter pulls from the plot size and plot number slots that are constants.

--- R/getSubplotCount.R
+++ R/getSubplotCount.R
@@ -14,42 +14,55 @@
 #'  \item{"count"}{A count of the number of the number of subplots/microplots/quadrats that the group is monitored in for each plot event.}
 #'}
 #' 
-#' @return For a single NPSForVeg object getSubplotCount returns a vector of length one. For a list of such object it returns a vector as long as the list. 
+#' @return For a single NPSForVeg object getSubplotCount returns a data.frame. 
 #' 
 #' @export
 
 ### This calculates the areas for various kinds of plots in m^2
 
-setGeneric(name="getSubplotCount",function(object,group,years=NA,plots=NA,subtype=NA,output="list",...){standardGeneric("getSubplotCount")},signature=c("object") )
+setGeneric(name="getSubplotCount",function(object,group,years=NA,plots=NA,subtype=NA,...){standardGeneric("getSubplotCount")},signature=c("object") )
 
 setMethod(f="getSubplotCount", signature="list",
   function(object,group,years,plots,subtype,...) 
   {OutSubplots<-lapply(X=object, FUN=getSubplotCount,group=group,subtype=subtype,years=years,plots=plots)
-  switch(output,
-    list=return(OutSubplots),
-    vector=return(unlist(OutSubplots)) #this isn't working right for me yet.
-    )
+    return(do.call("rbind",OutSubplots)) 
+    
   })
 
 setMethod(f="getSubplotCount", signature="NPSForVeg",
   function(object,group,years,plots,subtype,...){
 
-    XSubplots<-getEvents(object, plots, years)
+    XSubplots<-object@Events
+    if(!sum(is.na((years))>0) & !sum(is.na(plots))>0) {XSubplots<-XSubplots %>% filter(Plot_Name %in% plots , Event_Year %in% years)}
+    if(!sum(is.na((plots))>0) & !sum(is.na(years))==0) {XSubplots<-XSubplots %>% filter(Plot_Name %in% plots)}
+    if(!sum(is.na((years))>0) & !sum(is.na(plots))==0) {XSubplots<-XSubplots %>% filter(Event_Year %in% years)}
+    
 
     switch(group,
+      saplings = switch (subtype,
+        all= XSubplots<-ifelse("numSapPlots" %in% names(XSubplots), return(XSubplots%>% select(Event_ID,Plot_Name,Event_Year,numSapPlots) %>%     
+                        mutate(SapPlotAll=numSapPlots*object@SapPlotSize[2]) %>% select(-numSapPlots)), 
+                        return(XSubplots%>% select(Event_ID,Plot_Name,Event_Year) %>% mutate(SapPlotAll=object@SapPlotSize[1]*object@SapPlotSize[2]))),
+        single= return(object@SapPlotSize[2]),
+        count= XSubplots<-ifelse("numSapPlots" %in% names(XSubplots), return(XSubplots%>% select(Event_ID,Plot_Name,Event_Year,numSapPlots)),
+                        return(XSubplots%>% select(Event_ID,Plot_Name,Event_Year) %>% mutate(numSapPlots=object@SapPlotSize[1])))),
+      
       seedlings = switch (subtype,
-        all= return(object@SeedPlotSize[2]*XSubplots$nummicro),
+        all= XSubplots<-ifelse("numSeedPlots" %in% names(XSubplots), return(XSubplots%>% select(Event_ID,Plot_Name,Event_Year,numSeedPlots) %>%     
+                        mutate(SeedPlotAll=numSeedPlots*object@SeedPlotSize[2]) %>% select(-numSeedPlots)), 
+                        return(XSubplots %>% select(Event_ID,Plot_Name,Event_Year) %>% mutate(SeedPlotAll=object@SeedPlotSize[1]*object@SeedPlotSize[2]))),
         single= return(object@SeedPlotSize[2]),
-        count= return(XSubplots$nummicro) 
-      ),
-
+        count= XSubplots<-ifelse("numSeedPlots" %in% names(XSubplots), return(XSubplots%>% select(Event_ID,Plot_Name,Event_Year,numSeedPlots)),
+          return(XSubplots%>% select(Event_ID,Plot_Name,Event_Year) %>% mutate(numSeedPlots=object@SeedPlotSize[1])))),
+      
       herbs = switch (subtype,
-        all= return(object@HPlotSize[2]*XSubplots$numquads),
-        single= return(object@HPlotSize[2]),
-        count= return(XSubplots$numquads)
-      )
-    )
-    if(!sum(is.na((plots))>0) &!sum(is.na(years))==0) XSubplots<-(XSubplots[XSubplots$Plot_Name %in% plots,])
-    if(!sum(is.na((years))>0) &!sum(is.na(plots))==0) XSubplots<-(XSubplots[XSubplots$Event_Year %in% years,])
+        all= XSubplots<-ifelse("numHerbPlots" %in% names(XSubplots), return(XSubplots%>% select(Event_ID,Plot_Name,Event_Year,numHerbPlots) %>%     
+                        mutate(HerbPlotAll=numHerbPlots*object@HPlotSize[2]) %>%  select(-numHerbPlots)), 
+                        return(XSubplots %>% select(Event_ID,Plot_Name,Event_Year) %>% mutate(HerbPlotAll=object@HPlotSize[1]*object@HPlotSize[2]))),
+        single= return(object@HerbPlotSize[2]),
+        count= XSubplots<-ifelse("numHerbPlots" %in% names(XSubplots), return(object@Events%>% select(Event_ID,Plot_Name,Event_Year,numHerbPlots)),
+                        return(XSubplots %>% select(Event_ID,Plot_Name,Event_Year) %>% mutate(numHerbPlots=object@HPlotSize[1])))))
+        
+    return(XSubplots)
      })
 

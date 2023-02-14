aef464b3554bbccac1ea3a70fe4f876732dcf495
John Paul Schmit <jschmit@nps.gov>
2023-02-14 13:08:31 -0500

    Regen_Update_2023 4 - Added cycle fitler to getPlots() - Updated documenation - Partailly addresses #12

--- R/getPlots.R
+++ R/getPlots.R
@@ -13,6 +13,7 @@
 #' }
 #' @param visits A numeric vector. Returns only data from plots where the number of plot visits matches one of the values in \code{visits} The number of visits to a plot is determined by the \code{Event_Count} column in the \code{Events} slot.
 #' @param years A numeric vector. Returns only plot data from plots where the years the plot was visited  matches one of the values in \code{years}. The year a visit takes place is determined by the \code{Event_Year} column in the \code{Events} slot.
+#' @param cycles A numeric vector. Returns only plot data from plots where the cycle the plot was visited  matches one of the values in \code{cycles}. The cycle a visit takes place is determined by the \code{Cycle} column in the \code{Events} slot.
 #' @param subparks A character vector. Returns only data from plots where the subpark the plot is in matches one of the values in \code{subparks}. The subpark a plot is located in is determined by the \code{SubUnit_Code} column in the \code{Plots} slot.
 #' @param events A \code{data.frame} of events data like that produced by \code{getEvents}. Only used by the \code{data.frame} method when filtering by \code{year}. Typically this is automatically generated directly from the \code{NPSForVeg} objects. 
 #' @param output Either "dataframe" (the default) or "list". Note that this must be in quotes. Determines the type of output from the function. Only used when the input \code{object} is a list.
@@ -22,17 +23,17 @@
 #' @export
 
 
-setGeneric(name = "getPlots", function(object, type = "active", visits = NA, years = NA, plots = NA, subparks = NA, events = NA, output = "dataframe") {
+setGeneric(name = "getPlots", function(object, type = "active", visits = NA, years = NA, cycles=NA, plots = NA, subparks = NA, events = NA, output = "dataframe") {
   standardGeneric("getPlots")
 }, signature = "object")
 
 
 setMethod(
   f = "getPlots", signature = c(object = "list"),
-  function(object, type, visits, years, plots, subparks, output) {
+  function(object, type, visits, years, cycles, plots, subparks, output) {
     XPlots <- map(object, ~ `@`(.x, Plots))
 
-    events <- if (!anyNA(years)) {
+    events <- if (!anyNA(years) | !anyNA(cycles) ) {
       switch(output,
         list = map(object, ~ `@`(.x, Events)),
         dataframe = bind_rows(map(object, ~ `@`(.x, Events)))
@@ -40,8 +41,8 @@ setMethod(
     } else NA
 
     OutPlots <- switch(output,
-      list = map2(.x=XPlots, .y=events, .f= ~ getPlots(.x, type, visits, years, plots, subparks, events=.y)),
-      dataframe = getPlots(object = bind_rows(XPlots), type, visits, years, plots, subparks, events)
+      list = map2(.x=XPlots, .y=events, .f= ~ getPlots(.x, type, visits, years, cycles, plots, subparks, events=.y)),
+      dataframe = getPlots(object = bind_rows(XPlots), type, visits, years, cycles, plots, subparks, events)
     )
     return(OutPlots)
   }
@@ -50,15 +51,15 @@ setMethod(
 
 setMethod(
   f = "getPlots", signature = c(object = "NPSForVeg"),
-  function(object, type, visits, years, plots, subparks) {
+  function(object, type, visits, years, cycles, plots, subparks) {
     XPlots <- object@Plots
 
-    if (!anyNA(years)) {
+    if (!anyNA(years) |!anyNA(cycles)) {
       events <- object@Events
     }
 
     OutPlots <- getPlots(XPlots,
-      type = type, visits = visits, years = years, plots = plots, events = events,
+      type = type, visits = visits, years = years, cycles=cycles, plots = plots, events = events,
       subparks = subparks
     )
 
@@ -69,7 +70,7 @@ setMethod(
 
 setMethod(
   f = "getPlots", signature = c(object = "data.frame"),
-  function(object, type, visits, years, plots, subparks, events) {
+  function(object, type, visits, years, cycles, plots, subparks, events) {
     object <- switch(type,
       all = object,
       active = filter(object, Location_Status == "Active"),
@@ -83,6 +84,8 @@ setMethod(
     if (!anyNA(subparks)) object <- filter(object, Subunit_Code %in% subparks)
 
     if (!anyNA(years)) object <- semi_join(object, filter(events, Event_Year %in% years), by = "Plot_Name")
+    
+    if (!anyNA(cycles)) object <- semi_join(object, filter(events, Cycle %in% cycles), by = "Plot_Name")
 
     return(object)
   }
